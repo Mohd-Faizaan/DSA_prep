a0e1e064177940462163424efe3c6153ad22ca0c
Mohd-Faizaan <mohammadfaizaan2006@gmail.com>
2026-07-12 02:21:30 +0530

    Time: 19 ms (95.89%), Space: 82.7 MB (80.79%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
-
-        for (int n : nums) {
-            
-            
-            if (seen.count(n)) return true;
-            
-            seen.insert(n);
-        
+        sort(nums.begin(),nums.end());
+        for(int i=0 ; i < nums.size() -1; i++)
+        {
+            if ( nums[i] == nums[i+1])
+            {
+                return true;
+            }
         }
-
-      
-        return false;
     
-    
-    }
-
-    
+return false;    }
 };
