dd458d91b450498e3ea0c3c3cbcda6a3230ad22d
Mohd-Faizaan <mohammadfaizaan2006@gmail.com>
2026-07-23 15:30:02 +0530

    Time: 0 ms (100%), Space: 8.8 MB (41.99%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,40 @@
+//#include <string>
+//#include <vector>
+//#include <unordered_map>
+//#include <sstream>
+using namespace std;
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        stringstream ss(s);
+        string word;
+        while (ss >> word) {
+            words.push_back(word);
+        }
+
+        if (words.size() != pattern.length()) {
+            return false;
+        }
+
+        unordered_map<char, string> map_c_w;
+        unordered_map<string, char> map_w_c;
+
+        for (int i = 0; i < pattern.length(); ++i) {
+            char ch = pattern[i];
+            string& current_word = words[i];
+
+            if (map_c_w.count(ch) && map_w_c.count(current_word)) {
+                if (map_c_w[ch] != current_word || map_w_c[current_word] != ch) {
+                    return false;
+                }
+            } else if (map_c_w.count(ch) || map_w_c.count(current_word)) {
+                return false;
+            } else {
+                map_c_w[ch] = current_word;
+                map_w_c[current_word] = ch;
+            }
+        }
+        return true;
+    }
+};
