992810cf1c14253c8c0ace2587453e0cd821e435
Mohd-Faizaan <mohammadfaizaan2006@gmail.com>
2026-07-28 17:01:48 +0530

    Time: 2 ms (16.74%), Space: 9.3 MB (26.06%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -4,8 +4,7 @@ public:
     {
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        for(int i=0;i<s.size();i++)
-        {
+        for(int i=0;i<s.size();i++){
             if(s[i]!=t[i]) return t[i];
         }
         return t[s.size()];
